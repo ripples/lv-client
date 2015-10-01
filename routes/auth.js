1db4786114bef5ac24ac00c162d9960ec2cf3682
Ryan Stanley <stanleyrya@gmail.com>
2015-10-01 01:43:45 -0400

    Removed unused jade, started removing unused routing code, switch NetID to username to match server

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,4 @@ var express = require('express');
 var path = require("path");
 var router = express.Router();
 
-/* Authenticate a User */
-router.get('/auth', function(req, res, next){
-  res.send({token : "WEBTOKEN"});
-});
-
-router.get('/WEBTOKEN', function(req, res, next){
-  console.log('WEBTOKEN');
-  res.sendFile(path.join(__dirname+'/../views/main.html')); 
-
-});
-
 module.exports = router;
