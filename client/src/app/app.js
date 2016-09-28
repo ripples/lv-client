fa2f8cdd6f8148a88e9034b0cb97ce59ce564dd2
Dylan Fischler <dylanfischler@gmail.com>
2016-09-27 23:00:12 -0400

    Started some basic dev on the UI, added basic routing, test UI page, some early styling

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -2,15 +2,18 @@
 
 import React from "react";
 import {render} from "react-dom";
-// import {Router, Route, IndexRoute, browserHistory} from "react-router";
+import {Router, Route, browserHistory} from "react-router";
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import thunkMiddleware from "redux-thunk";
 
 import appReducer from "./reducers/app";
 
-// Hello World
-import HelloWorld from "./components/HelloWorld/HelloWorld";
+// core App component
+import App from "./pages/App/App";
+
+// ui demonstration component
+import UI from "./pages/UI/ui";
 
 const mountNode = document.getElementById("lvapp");
 
@@ -20,8 +23,12 @@ const store = createStore(
   applyMiddleware(thunkMiddleware)
 );
 
+/* note: UI component will be used to develop/test our base styles, and will be removed before production */
 render((
   <Provider store={store}>
-    <HelloWorld />
+    <Router history={browserHistory}>
+        <Route path="/" component={App}></Route>
+        <Route path="/ui" component={UI}></Route>
+    </Router>
   </Provider>
 ), mountNode);
