0f1cd142480dd38c5d4dbe6e312295e7fe9afdde
Dylan Fischler <dylanfischler@gmail.com>
2016-10-14 00:01:28 -0400

    added some color coding

--- client/src/app/components/CourseItem/CourseItem.js
+++ client/src/app/components/CourseItem/CourseItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 import LectureItem from "components/LectureItem/LectureItem";
+import Colors from "constants/ColorConstants";
 
 class CourseItem extends React.Component {
   render() {
     const NUM_LECTURES = 5;
+    const colorList = [Colors.BLUE, Colors.PURPLE, Colors.YELLOW, Colors.GREEN];
 
     return (
       <div className="course-item">
+        <span className="color-code" style={{
+          backgroundColor: colorList[Math.floor(Math.random() * (colorList.length - 0)) + 0]
+        }} />
         <h4>{this.props["course-data"].title}</h4>
         <div className="lecture-list">
           {this.props.lectures.slice(0, NUM_LECTURES).map((lecture, i) => {
---
+++ client/src/app/constants/ColorConstants.js
@@ -0,0 +1,6 @@
+export default {
+  BLUE: "#3ba5db",
+  YELLOW: "#f49800",
+  PURPLE: "#a478ce",
+  GREEN: "#85b500"
+};
