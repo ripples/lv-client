66e0137eb4fe3a1e98b369fb3b143116a9d669a7
Dylan Fischler <dylanfischler@gmail.com>
2016-10-26 21:18:46 -0400

    added a kind of decent lecture media page

--- client/src/app/pages/App/Lecture/Lecture.js
+++ client/src/app/pages/App/Lecture/Lecture.js
@@ -1,11 +1,24 @@
 import React from "react";
 import {connect} from "react-redux";
+import moment from "moment";
+
+import LectureMedia from "components/LectureMedia/LectureMedia";
 
 class Lecture extends React.Component {
   render() {
     return (
       <div className="lecture">
-        <h1>{this.props.lecture.title}</h1>
+        <div className="lecture-header">
+          <h1>
+            {this.props.course.title.split(":")[0]}: {this.props.lecture.title}
+          </h1>
+          <h4>
+            {moment(this.props.lecture.date).format("dddd, MMMM Do YYYY")}
+          </h4>
+        </div>
+        <div className="lecture-body">
+          <LectureMedia lecture={this.props.lecture} />
+        </div>
       </div>
     );
   }
@@ -13,12 +26,14 @@ class Lecture extends React.Component {
 
 Lecture.propTypes = {
   params: React.PropTypes.object,
-  lecture: React.PropTypes.object.isRequired
+  lecture: React.PropTypes.object.isRequired,
+  course: React.PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    lecture: Object.assign({}, state.lectures[ownProps.params.lectureId])
+    lecture: Object.assign({}, state.lectures[ownProps.params.lectureId]),
+    course: state.courses.find(course => course.id === ownProps.params.courseId)
   };
 };
 
