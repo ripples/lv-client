8b1621bfcfdb6b1b0ac8a973ffba3c94eadd531b
Dylan Fischler <dylanfischler@gmail.com>
2016-11-02 23:39:35 -0400

    Login working

--- client/src/app/components/Header/header.js
+++ client/src/app/components/Header/header.js
@@ -2,14 +2,33 @@ import React from "react";
 import {Link} from "react-router";
 import Logo from "components/Logo/logo";
 
+import {logout} from "libs/auth";
+
 class Header extends React.Component {
+  doLogout() {
+    logout();
+    this.context.router.push("/login");
+  }
+
   render() {
     return (
       <div className="header">
-        <Link to="/"><Logo /></Link>
+        <div className="left">
+          <Link to="/"><Logo /></Link>
+        </div>
+        <div className="right">
+          {
+            (this.context.router.isActive("/login"))
+              ? null : <button className="small" onClick={() => this.doLogout()}>Logout</button>
+          }
+        </div>
       </div>
     );
   }
 }
+
+Header.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default Header;
