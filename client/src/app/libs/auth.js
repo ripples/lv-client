f1336bffda624d581ddf3ee7a1399326cc16eacf
Cheyan Setayesh <cheyan.set@gmail.com>
2016-12-02 01:35:00 -0500

    add unstyled reset forms

--- client/src/app/libs/auth.js
+++ client/src/app/libs/auth.js
@@ -27,6 +27,62 @@ export function requireAuth(nextState, replace, callback) {
 }
 
 /**
+ * Reset password
+ * @param {String} token - reset token provided in email
+ * @param {String} email - email to reset for
+ * @param {String} password - new password
+ * @return {Promise} returns Promise
+ */
+export function resetPassword(token, email, password) {
+  return new Promise((resolve, reject) => {
+    request(new Request(`${BASE_URL}/login/reset`, {
+      method: "POST",
+      body: JSON.stringify({token, email, password}),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    }), (err, data) => {
+      if (err) {
+        reject({
+          payload: err
+        });
+      } else {
+        resolve({
+          payload: data
+        });
+      }
+    });
+  });
+}
+
+/**
+ * Send password reset email
+ * @param {String} email - user email
+ * @return {Promise} returns Promise
+ */
+export function requestResetEmail(email) {
+  return new Promise((resolve, reject) => {
+    request(new Request(`${BASE_URL}/login/forgot`, {
+      method: "POST",
+      body: JSON.stringify({email}),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    }), (err, data) => {
+      if (err) {
+        reject({
+          payload: err
+        });
+      } else {
+        resolve({
+          payload: data
+        });
+      }
+    });
+  });
+}
+
+/**
  * Login user
  * @param {String} email - user email
  * @param {String} password - user password
@@ -36,10 +92,7 @@ export function login(email, password) {
   return new Promise((resolve, reject) => {
     request(new Request(`${BASE_URL}/login`, {
       method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password
-      }),
+      body: JSON.stringify({email, password}),
       headers: new Headers({
         "Content-Type": "application/json"
       })
