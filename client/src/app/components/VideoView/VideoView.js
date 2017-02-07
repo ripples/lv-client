ffd421c68225d8dace153da32e74dd8bff5eacb5
jtcoon <justin.tyler.coon@gmail.com>
2017-02-07 17:46:36 -0500

    added videojs

--- client/src/app/components/VideoView/VideoView.js
+++ client/src/app/components/VideoView/VideoView.js
@@ -1,26 +1,21 @@
 import React from "react";
+import {BASE_URL} from "../../constants/ApiConstants";
+import VideoPlayer from "../VideoPlayer/VideoPlayer";
 
 class VideoView extends React.Component {
-
-  sayVideoTimer() {
-    var video = document.getElementById('player');
-    console.log(video.currentTime);
-  }
-
   render() {
+    const videoJsOptions = {
+      autoPlay: true,
+      controls: true,
+      sources: [{
+        src: BASE_URL + "/a/F16/COMPSCI 220/08-29-2016--08-59-01/video",
+        type: "video/mp4"
+      }]
+    };
     return (
       <div className="video-view">
         <div className="video">
-          <video id="player"
-                 className="video-js vjs-default-skin"
-                 src="http://192.168.99.100/api/v1/videotest/video"
-                 type="video/mp4"
-                 height="50%"
-                 width="50%"
-                 controls preload="auto">
-          </video>
-          <img src="http://192.168.99.100/api/v1/videotest/F16/COMPSCI 220/08-29-2016--08-59-01/images/whiteboard/full/whiteBoard-0-1472475961" width="50%" height="50%"></img>
-          <button onClick={this.sayVideoTimer}>click me</button>
+          <VideoPlayer height="400px" width="400px" { ...videoJsOptions } />
         </div>
       </div>
     );
