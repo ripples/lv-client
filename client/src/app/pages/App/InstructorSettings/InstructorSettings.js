c5bf2b43c1d834bcef8d23b4479da8af1602395c
Ryan Jerue <ryan@jerue.org>
2017-02-16 23:09:13 -0500

    dammit webpack...

--- client/src/app/pages/App/InstructorSettings/InstructorSettings.js
+++ client/src/app/pages/App/InstructorSettings/InstructorSettings.js
@@ -1,12 +1,12 @@
 import React from "react";
 import InstructorCourseList from "../../../components/InstructorCourseList/InstructorCourseList";
-import Link from "react-router";
+import {Link} from "react-router";
 
 class InstructorSettings extends React.Component {
   render() {
     return (
       <div className="instructor-settings">
-        <Link to="/courses">My Courses</Link> <span>/</span>
+        <Link to="/courses">My Courses</Link>
         <h1>Instructor Settings</h1>
         <InstructorCourseList />
       </div>
