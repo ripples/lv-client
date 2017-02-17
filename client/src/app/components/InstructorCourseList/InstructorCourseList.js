c93cb8abf729908eefe2ac82d81706334b485188
Rajesh Susai <rajeshsusai14@gmail.com>
2017-02-17 00:57:42 -0500

    UI progress

--- client/src/app/components/InstructorCourseList/InstructorCourseList.js
+++ client/src/app/components/InstructorCourseList/InstructorCourseList.js
@@ -1,25 +1,33 @@
 import React from "react";
 import {connect} from "react-redux";
+import {InstructorCourseItem} from "components/InstructorCourseItem/InstructorCourseItem";
 
 class InstructorCourseList extends React.Component {
+  getCourseLectures(lectureKeys) {
+    // resolve lecture items by key
+    // return lectureKeys.map(lectureKey => Object.assign({}, this.props.lectures[lectureKey], {lectureId: lectureKey}));
+    return this.props.lectures.filter(lecture => lectureKeys.indexOf(lecture.lectureId) >= 0);
+  }
   render() {
     return (
-      <div className="something">
-        ahah
-      </div>
+      <ul className="instructor-courselist">
+        {this.props.courses.map((course, i) => {
+          let lectures = this.getCourseLectures(course.lectures);
+          return <li key={i}><InstructorCourseItem course-data={course}/></li>;
+        })
+        }
+      </ul>
     );
   }
 }
 
 InstructorCourseList.propTypes = {
-  courses: React.PropTypes.array,
-  lectures: React.PropTypes.array
+  courses: React.PropTypes.array
 };
 
 const mapStateToProps = state => {
   return {
-    courses: state.courses,
-    lectures: state.lectures
+    courses: state.courses
   };
 };
 
