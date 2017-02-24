49af67690d91c8824731d07f5bc10b4601a7b26e
Rajesh Susai <rajeshsusai14@gmail.com>
2017-02-24 01:27:11 -0500

    Styled the Instructor Course Settings Page even more, two headers aren't styling for some reason.

--- client/src/app/components/InstructorCourseItem/InstructorCourseItem.js
+++ client/src/app/components/InstructorCourseItem/InstructorCourseItem.js
@@ -2,18 +2,14 @@ import React from "react";
 
 class InstructorCourseItem extends React.Component {
   render() {
-    var textbox = {
-      width: 600,
-      height: 149
-    };
     return (
       <div className="instructor-course-item">
         <div className="rectangle-big">
           <div className="course-header">{this.props.data.title}</div>
           <div className="course-title">COURSE TITLE</div>
-          <textarea type="text" placeholder={this.props.data.title}></textarea>
+          <input name="course-title-text" type="text" placeholder={this.props.data.title}></input>
           <div className="course-description">COURSE DESCRIPTION</div>
-          <textarea type="text" style={textbox}></textarea>
+          <textarea name="course-description-text" type="text"></textarea>
         </div>
       </div>
     );
